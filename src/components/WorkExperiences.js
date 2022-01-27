26018c0e54920a8b21f9806c238a9e80fc010c0b
Siddharth <siddharth.mishra@publicissapient.com>
2022-01-27 21:04:59 +0530

    First commit

---
+++ src/components/WorkExperiences.js
@@ -0,0 +1,38 @@
+import { BadgeCheckIcon, BriefcaseIcon } from "@heroicons/react/solid";
+import React from "react";
+import { workExperiences } from "../data";
+
+export default function WorkExperience() {
+  return (
+    <section id="workExperiences">
+      <div class="container px-5 py-10 mx-auto">
+        <div class="text-center mb-20">
+          <BriefcaseIcon class="w-10 inline-block mb-4" />
+          <h1 class="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
+            Work Experience
+          </h1>
+          {/* <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
+            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
+            possimus est.
+          </p> */}
+        </div>
+        <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+          {workExperiences.map((workExperience) => (
+            <div key={workExperience} class="p-2 sm:w-1/2 w-full">
+              <div class="bg-gray-800 rounded flex p-4 h-full items-center">
+                <BadgeCheckIcon class="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
+                <span class="title-font font-medium text-white ml-5">
+                  {workExperience.companyName}
+                </span>
+                <span class="title-font font-medium ml-5">
+                  {workExperience.duration}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const projects = [
+    {
+      title: "ARABA",
+      subtitle: "Food Delivery App",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: require('./images/arabacollage.jpeg'),
+      link: "https://play.google.com/store/apps/details?id=com.araba.ksa.arabaksa",
+    },
+    {
+      title: "NAJEZ",
+      subtitle: "Cab Booking App",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: require('./images/najezcollage.jpeg'),
+      link: "http://www.globtierinfotech.com/portfolio/najez-app.html",
+    },
+    {
+      title: "LIVE SPORTS",
+      subtitle: "Score Prediction App",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: require('./images/livesportcollage.jpeg'),
+      link: "https://play.google.com/store/apps/details?id=com.thetechtonic.livesports&hl=en",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
+
+  export const skills = [
+      {
+          title: 'Core Java', 
+          image: require('./images/javaicon.png')
+        },
+        {
+            title: 'Android', 
+            image: require('./images/androidicon.png')
+          },
+          {
+            title: 'Javascript', 
+            image: require('./images/jsicon.png')
+          },
+          {
+            title: 'React', 
+            image: require('./images/reacticon.png')
+          },
+          {
+            title: 'React Native', 
+            image: require('./images/reactnativeicon.png')
+          },
+          {
+            title: 'Redux', 
+            image: require('./images/reduxicon.png')
+          }];
+
+  export const workExperiences = [
+      {
+          companyName: 'AcumenCog UG', 
+          duration: 'August 2019 - Present'
+        },
+        {
+            companyName: 'The Techtonic', 
+            duration: 'March 2018 - August 2019'
+          },
+          {
+            companyName: 'TechconLabs Pvt Ltd,', 
+            duration: 'November 2016 - March 2018'
+          },
+          {
+            companyName: 'Cabofy Voyage Services Pvt Ltd', 
+            duration: 'July 2015 - May 2016'
+          },
+    ];
