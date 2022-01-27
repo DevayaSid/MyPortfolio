73263ef647ed5dffcf9cac81ad243958593e8c4c
Siddharth <siddharth.mishra@publicissapient.com>
2022-01-27 21:40:54 +0530

    removed warnings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <header class="bg-gray-800 md:sticky top-0 z-10">
       <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a class="title-font font-medium text-white mb-4 md:mb-0">
+        <a href={() => false} class="title-font font-medium text-white mb-4 md:mb-0">
           <a href="#about" class="ml-3 text-xl">
             Siddharth Mishra
           </a>
